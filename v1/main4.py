9f88e51e8686a632018282eed0a1fab81217aa92
dishin <dishin@jlkgroup.com>
2022-02-11 13:18:09 +0900

    git test 파일 정리 mysql new version copy (이관 전 레파지토리에서 가져옴)

--- v1/main4.py
+++
@@ -1,5 +0,0 @@
-from service.KRW_ALL import *
-
-
-if __name__ == "__main__":
-    catch_krw_new_public()
--- v1/main6.py
+++
@@ -1,6 +0,0 @@
-from service.KRW_ALL import *
-
-
-if __name__ == "__main__":
-
-    cross_state_for_all_KRW()
